4dd98abba97f5efb24bd32444e15c11030b84b25
2355456062@qq.com <2355456062@qq.com>
2019-04-10 17:38:20 +0800

    feat(销售单模块): 新增销售单列表和详情 增加二维码组件 选择组件

---
+++ pages/online-record/index.vue
@@ -0,0 +1,342 @@
+<template>
+	<view>
+		<mpvue-picker ref="mpvueTimePicker" :pickerValueDefault="timeIndex" mode='selector' @onConfirm="onTimeConfirm"
+		 :pickerValueArray="timeArray"></mpvue-picker>
+		 <mpvue-picker ref="mpvueStatusPicker" :pickerValueDefault="statusIndex" mode='selector' @onConfirm="onStastuConfirm"
+		  :pickerValueArray="statusArray"></mpvue-picker>
+		<view class="transaction-selection">
+			<view class="time" @tap="showPicker('time')">
+				<text>{{timeArray[timeIndex[0]].label}}</text>
+				<view class="arrow-big"></view>
+				<view class="arrow-small"></view>
+			</view>
+			<view class="line"></view>
+			<view class="status" @tap="showPicker('status')">
+				<text>{{statusArray[statusIndex[0]].label}}</text>
+				<view class="arrow-big"></view>
+				<view class="arrow-small"></view>
+			</view>
+		</view>
+		<view class="transaction-list">
+			<view class="transaction-list-item" v-for="(item, index) in list" :key="index">
+				<navigator class="navigator" :url="'/pages/online-record/info?paymentStatus='+item.paymentStatus+'&saleOrder='+item.saleOrderNum"
+				 hover-class="navigator-hover">
+					<view class="item-start">
+						<view>{{item.day}}</view>
+						<view class="month">{{item.month}}</view>
+					</view>
+					<view class="item-middle">
+						<view>{{item.saleOrderNum}}</view>
+						<view class="status" :style="item.paymentStatus == 1?'color: #6BC892;':''">{{item.paymentStatusType}}</view>
+					</view>
+					<view class="item-end">
+						<view class="money">{{item.money}}</view>
+						<view>{{item.time}}</view>
+						<view class="arrow-big"></view>
+						<view class="arrow-small"></view>
+					</view>
+				</navigator>
+			</view>
+		</view>
+		<uni-load-more :status="loading"></uni-load-more>
+
+	</view>
+</template>
+
+<script>
+	import {
+		formatTime,
+		formatSubtractTime
+	} from '@/common/libs/util/time.js'
+	import {
+		getArrValue
+	} from "@/common/libs/util/index.js"
+	import {
+		getSaleOrderList
+	} from '@/common/api/saleOrder/index.js';
+	import mpvuePicker from "@/components/mpvue-picker/mpvue-picker.vue"
+	import uniLoadMore from "@/components/uni-load-more/uni-load-more.vue"
+	export default {
+		data() {
+			return {
+				timeArray: [{
+					label: '当日',
+					value: 0
+				}, {
+					label: '最近7天',
+					value: 7
+				}, {
+					label: '最近30天',
+					value: 30
+				}],
+				statusArray: [{
+					label: '全部',
+					value: -1
+				}, {
+					label: '待支付',
+					value: 0
+				}, {
+					label: '已扫码，待支付',
+					value: 2
+				}, {
+					label: '已支付',
+					value: 1
+				}],
+				timeIndex: [0],
+				statusIndex: [0],
+				list: [],
+				// 是否在搜索
+				isSearch: false,
+
+				loading: 'more',
+				paymentStatusList: [{
+						key: 0,
+						value: '代支付'
+					},
+					{
+						key: 1,
+						value: '已支付'
+					},
+					{
+						key: 2,
+						value: '已扫码，待支付'
+					}
+				],
+				params: {
+					orderNumber: '',
+					customerName: '',
+					state: -1,
+					tradeMode: -1,
+					paymentStatus: -1,
+					orderSource: -1,
+					salesman: '',
+					companyId: '',
+					pageSize: 10,
+					pageNum: 1,
+					total: 0,
+					auditState: -1,
+					logisticsSheet: -1,
+					printing: -1,
+					stateTime: -1,
+					sTime: formatTime(),
+					eTime: formatTime()
+				}
+			}
+		},
+		components: {
+			uniLoadMore,
+			mpvuePicker
+		},
+		computed: {},
+		onLoad() {
+			console.log('onload');
+			// this.list = [];
+			this.params.pageNum = 1;
+			this.getList();
+		},
+		onReachBottom() {
+			console.log('onbuttom');
+			this.getList();
+		},
+		methods: {
+			onTimeConfirm(e) {
+				this.timeIndex = e.index;
+				this.params.eTime = formatSubtractTime(e.value[0]);
+				this.params.pageNum = 1;
+				this.list = [];
+				this.isSearch = false;
+				this.getList();
+			},
+			onStastuConfirm(e){
+				console.log(e)
+				this.statusIndex = e.index;
+				this.params.paymentStatus = e.value[0];
+				this.params.pageNum = 1;
+				this.list = [];
+				this.isSearch = false;
+				this.getList();
+			},
+			showPicker(type) {
+				if(type == 'time'){
+					this.$refs.mpvueTimePicker.show();
+				}else {
+					this.$refs.mpvueStatusPicker.show();
+				}
+				
+			},
+			getList() {
+				let params = {
+					...this.params
+				}
+				// 在搜索中不再执行
+				if (this.isSearch) {
+					return;
+				}
+				if(this.list.length == this.params.total && this.list.ength != 0){
+					return;
+				}
+				this.loading = 'loading';
+				this.isSearch = true;
+				getSaleOrderList(params).then(res => {
+					this.isSearch = false;
+					this.params.pageNum = this.params.pageNum + 1;
+					this.params.total = res.data.total || 0;
+					let list = res.data.list || [];
+					list.forEach(item => {
+						item.month = formatTime(item.createdAt, "MM月");
+						item.day = formatTime(item.createdAt, "DD");
+						item.time = formatTime(item.createdAt, "HH:mm:SS");
+						if (item.money) {
+							item.money = '￥' + item.money;
+						}
+						item.paymentStatusType = getArrValue(this.paymentStatusList, item.paymentStatus);
+						this.list.push(item);
+					})
+					if (res.data.total == 0 || res.data.total == this.list.length) {
+						this.loading = 'noMore'
+					} else {
+						this.loading = 'more'
+					}
+				}).catch(error => {
+					this.isSearch = false;
+					this.loading = 'noMore';
+					console.log(error)
+				})
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	page {
+		min-height: 100%;
+		background: #f4f4f4;
+	}
+
+	.transaction-selection {
+		display: flex;
+		flex: 1;
+		align-items: center;
+		justify-content: space-between;
+		background: #fff;
+
+		& .line {
+			width: 2upx;
+			height: 40upx;
+			background: #ccc;
+		}
+
+		& .time,
+		& .status {
+			width: 374upx;
+			padding: 20upx 0;
+			font-size: 28upx;
+			color: #444;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			position: relative;
+
+			& .arrow-big {
+				width: 0;
+				height: 0;
+				border-width: 12upx;
+				border-style: solid;
+				border-color: #979797 transparent transparent transparent;
+				position: absolute;
+				top: 35upx;
+				right: 40upx;
+			}
+
+			& .arrow-small {
+				width: 0;
+				height: 0;
+				border-width: 10upx;
+				border-style: solid;
+				border-color: #fff transparent transparent transparent;
+				position: absolute;
+				top: 35upx;
+				right: 42upx;
+			}
+		}
+	}
+
+	.transaction-list {
+		margin-top: 10upx;
+		background: #fff;
+
+		&-item {
+			border-bottom: 5upx solid #f4f4f4;
+
+			& .navigator {
+				padding: 24px 0;
+				display: block;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				flex: 1;
+			}
+		}
+	}
+
+	.item-start {
+		width: 120upx;
+		text-align: center;
+		font-size: 40upx;
+		color: #666;
+
+		& .month {
+			font-size: 24upx;
+			color: #999;
+		}
+	}
+
+	.item-middle {
+		width: 380upx;
+		font-size: 26upx;
+		color: #444;
+
+		.status {
+			margin-top: 12upx;
+			font-size: 22upx;
+			color: #FC8351;
+		}
+	}
+
+	.item-end {
+		width: 250upx;
+		font-size: 24upx;
+		color: #BBBBBB;
+		padding-right: 100upx;
+		text-align: right;
+		position: relative;
+
+		& .money {
+			margin-bottom: 8upx;
+			font-size: 28upx;
+			color: #EE6060;
+		}
+
+		& .arrow-big {
+			width: 0;
+			height: 0;
+			border-width: 12upx;
+			border-style: solid;
+			border-color: transparent transparent transparent #979797;
+			position: absolute;
+			top: 32upx;
+			right: 38upx;
+		}
+
+		& .arrow-small {
+			width: 0;
+			height: 0;
+			border-width: 10upx;
+			border-style: solid;
+			border-color: transparent transparent transparent #fff;
+			position: absolute;
+			top: 34upx;
+			right: 42upx;
+		}
+	}
+</style>
---
+++ pages/online-record/info.vue
@@ -0,0 +1,334 @@
+<template>
+	<view class="order-info">
+		<view class="info-top" v-if="!isShowProductInfo">
+			<view class="money">{{info.paymentInfo && info.paymentInfo.tradingTotalAmount? '￥'+info.paymentInfo.tradingTotalAmount : ''}}</view>
+			<view class="state">{{paymentStatusName}}</view>
+			<view class="info-top-item">
+				<text class="title">订单号：</text>
+				<text>{{params.saleOrder}}</text>
+			</view>
+			<view class="info-top-item">
+				<text class="title">发货方式：</text>
+				<text>{{info.baseInfo && info.baseInfo.deliveryTypeName?info.baseInfo.deliveryTypeName:''}}</text>
+			</view>
+			<view class="info-top-item">
+				<text class="title">下单时间：</text>
+				<text>{{info.baseInfo && info.baseInfo.createdAtTime?info.baseInfo.createdAtTime:''}}</text>
+			</view>
+			<view class="info-top-item">
+				<text class="title">下单账号：</text>
+				<text>{{info.baseInfo && info.baseInfo.buyerId?info.baseInfo.buyerId:''}}</text>
+			</view>
+			<view class="qrcode" v-if="info.baseInfo && info.baseInfo.qrCode">
+				<uni-app-qrcode :size="318" :val="info.baseInfo.qrCode" :loadMake="true" :onval="true" />
+			</view>
+		</view>
+		<view :class="isShowProductInfo?'product product-change':'product'">
+			<view class="product-title" @tap="changeShowInfo">
+				<text class="title">商品信息</text>
+				<view class="arrow-big"></view>
+				<view class="arrow-small"></view>
+			</view>
+			<view class="product-list">
+				<view class="product-list-item" v-for="(item, index) in info.productInfo" :key="index">
+					<view class="item-head">
+						<image v-if="item.masterImage" class="img" mode="widthFix" :src="item.masterImage"></image>
+						<text class="subject">{{item.subject}}</text>
+					</view>
+					<view class="item-info">
+						<view class="spec">{{item.specInfo}}</view>
+						<view class="price">
+							<text>{{item.price}}</text>
+							<text>数量：{{item.orderNum}}</text>
+							<text class="red">{{'￥'+item.money}}</text>
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="product-total">
+				总计：
+				<text>货品：<text class="red">{{info.productInfoTotal && info.productInfoTotal.species?info.productInfoTotal.species:0}}种</text></text>
+				<text>数量：<text class="red">{{info.productInfoTotal && info.productInfoTotal.piece?info.productInfoTotal.piece:0}}种</text></text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		formatTime
+	} from '@/common/libs/util/time.js';
+	import {
+		getArrValue
+	} from "@/common/libs/util/index.js";
+	import {
+		getSaleOrderInfo
+	} from '@/common/api/saleOrder/index.js';
+	import uniAppQrcode from "@/components/uni-app-qrcode/uni-app-qrcode.vue"
+	export default {
+		data() {
+			return {
+				paymentStatusName: '',
+				isShowProductInfo: false,
+				params: {
+					state: 0,
+					saleOrder: ''
+				},
+				info: {
+					productInfo: [],
+					productInfoTotal: {
+						species: 0,
+						piece: 0
+					},
+					baseInfo: {}
+				},
+				paymentStatusList: [{
+						key: 0,
+						value: '代支付'
+					},
+					{
+						key: 1,
+						value: '已支付'
+					},
+					{
+						key: 2,
+						value: '已扫码，待支付'
+					}
+				]
+			}
+		},
+		components: {
+			uniAppQrcode
+		},
+		computed: {},
+		onLoad(option) {
+			console.log(option)
+			this.params.saleOrder = option.saleOrder;
+			let paymentStatus = option.paymentStatus || 0;
+			let paymentStatusList = [{
+					key: 0,
+					value: '代支付'
+				},
+				{
+					key: 1,
+					value: '已支付'
+				},
+				{
+					key: 2,
+					value: '已扫码，待支付'
+				}
+			];
+			this.paymentStatusName = getArrValue(this.paymentStatusList, paymentStatus);
+			this.getInfo();
+		},
+		onReachBottom() {},
+		methods: {
+			changeShowInfo() {
+				this.isShowProductInfo = !this.isShowProductInfo;
+			},
+			getInfo() {
+				let parasm = {
+					...this.params
+				}
+				getSaleOrderInfo(params).then(res => {
+					let info = res.data;
+					info.baseInfo.createdAtTime = formatTime(info.baseInfo.created, "YYYY-MM-DD HH:mm:SS");
+					info.baseInfo.deliveryTypeName = info.baseInfo.deliveryType == 1 ? '自提' : '物流';
+					this.info = info;
+				})
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	page {
+		min-height: 100%;
+		background: #f4f4f4;
+	}
+
+	.order-info {
+		position: relative;
+	}
+
+	.info-top {
+		.money {
+			font-size: 40upx;
+			color: #444444;
+			line-height: 56upx;
+			padding-top: 30upx;
+			text-align: center;
+		}
+
+		.state {
+			font-size: 26upx;
+			color: #FF8609;
+			text-align: center;
+			margin-top: 16upx;
+			line-height: 36upx;
+			padding-bottom: 30upx;
+		}
+
+		.qrcode {
+			background: #fff;
+			border: 1upx solid #E7E7E7;
+			width: 318upx;
+			margin: 17upx auto 0;
+			padding: 22upx 24upx;
+		}
+
+		.info-top-item {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			font-size: 26upx;
+			color: #444444;
+			padding: 16upx 30upx 0;
+
+			.title {
+				color: #999;
+			}
+		}
+	}
+
+	.product {
+		position: fixed;
+		bottom: 0;
+		height: 82upx;
+		overflow: hidden;
+		width: 100%;
+		z-index: 1000;
+
+		.product-title {
+			height: 82upx;
+			font-size: 30upx;
+			background: #fff;
+			color: #444444;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 30upx;
+			position: relative;
+
+			.title {
+				display: block;
+				padding-left: 24upx;
+				line-height: 36upx;
+				border-left: 10upx solid #4786ED;
+			}
+
+			& .arrow-big {
+				width: 0;
+				height: 0;
+				border-width: 16upx;
+				border-style: solid;
+				border-color: transparent transparent #979797 transparent;
+				position: absolute;
+				top: 22upx;
+				right: 40upx;
+			}
+
+			& .arrow-small {
+				width: 0;
+				height: 0;
+				border-width: 14upx;
+				border-style: solid;
+				border-color: transparent transparent #fff transparent;
+				position: absolute;
+				top: 26upx;
+				right: 42upx;
+			}
+		}
+	}
+
+	.product-change {
+		height: auto;
+		top: 0upx;
+
+		.product-title {
+			& .arrow-big {
+				border-color: #979797 transparent transparent transparent;
+				top: 26upx;
+				right: 40upx;
+			}
+
+			& .arrow-small {
+				border-color: #fff transparent transparent transparent;
+				top: 26upx;
+				right: 42upx;
+			}
+		}
+	}
+
+	/*  #ifdef  H5  */
+	.product-change {
+		top: 80upx;
+	}
+
+	/*  #endif  */
+	.product-list {
+		margin-top: 2upx;
+
+		.product-list-item {
+			background: #fff;
+			padding: 22upx 30upx 16upx;
+
+			.item-head {
+				display: flex;
+				align-items: flex-start;
+				justify-content: flex-start;
+
+				.img {
+					width: 100upx;
+					margin-right: 36upx;
+				}
+
+				.subject {
+					font-size: 26upx;
+					color: #444444;
+					line-height: 36upx;
+				}
+			}
+
+			.item-info {
+				margin-top: 20upx;
+				background: #F7FCFF;
+				padding: 12upx 24upx 10upx;
+				color: #666;
+				font-size: 12upx;
+
+				.spec {
+					line-height: 34upx;
+				}
+
+				.price {
+					margin-top: 10upx;
+					line-height: 34upx;
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+
+				}
+			}
+		}
+	}
+
+	.product-total {
+		margin-top: 104upx;
+		background: #FFFFFF;
+		border: 0 solid #DDDDDD;
+		font-size: 34upx;
+		color: #444444;
+		padding: 24upx 32upx 26upx;
+		line-height: 48upx;
+
+		&>text {
+			padding-left: 40upx;
+			font-size: 26upx;
+		}
+	}
+
+	.red {
+		color: #EE6060;
+	}
+</style>
